bad679ec53a685c6d665677c12a14f0e0e6dab30
ahinri <afhinrichsen@uc.cl>
2015-10-11 18:36:01 -0300

    comentarios main

--- AcademicManager/src/Main.java
+++ AcademicManager/src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("hola mundo");
+		/*
+		Por simplificacion, en esta entrega no se implementa serializacion
+		Debido a esto, los constructores y asignaciones a los atributos corresponden a nuevos objetos
+		Esto será reemplazado para la próxima entrega
+
+		Por ejemplo
+		Entrega3: List<HistorialAcademico> historialAcademicos= new List<HistorialAcademico>();
+		Entrega4: List<HistorialAcademico> historialAcademicos= deserializarHistoriales();
+		 */
+
+		/*
+		Hay 2 casos de uso que pueden ser probados:
+			1. Como alumno, crear un historial y agregar semestres, para poder hacer una comparacion con la malla asignada
+				*el resultado de esto seria ver el avance curricular (como en el siding)
+			2. Como admin, poder crear distintas entidades
+		 */
 
 	}
 
