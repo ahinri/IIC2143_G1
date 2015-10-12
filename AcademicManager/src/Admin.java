2da343f3eb96a1f9baa98b14e1955fd34095e2c6
fmsandoval <fmsandoval@uc.cl>
2015-10-12 20:39:02 -0300

    Admin y Ramo

--- AcademicManager/src/Admin.java
+++ AcademicManager/src/Admin.java
@@ -2,5 +2,34 @@
  * Created by ahinri on 11-10-2015.
  */
 public class Admin extends Usuario {
+	
+	public void crearMalla(String opcion)
+	{
+		int max_creditos_semestre = 50; //cambiar a leer valor
+		String facultad = "Ingenieria"; //cambiar a leer valor
+		int max_ramos_reprobados = 10; //cambiar a leer valor
+		Malla nueva = new Malla(max_creditos_semestre, facultad, max_ramos_reprobados);
+		boolean agregandoRamos = true;
+		while (agregandoRamos) //poner condicion de termino
+		{
+			Ramo r; //cambiar a seleccionar ramo
+			nueva.agregar_ramos(r);
+		}
+		//agregarla a un catalogo de mallas?
+	}
+	
+	public void crearRamo()
+	{
+		String horario = "L3"; //cambiar a leer valor
+		String sala = "A1"; //cambiar a leer valor
+		int seccion = 1; //cambiar a leer valor
+		int cupos = 50; //cambiar a leer valor
+		int anio = 2016; //cambiar a leer valor
+		int num_semestre = 1; //cambiar a leer valor
+		Profesor profesor; //cambiar a seleccionar profesor
+		DescriptorRamo descriptor; //cambiar a seleccionar descriptor
+		Ramo nuevo = new Ramo(horario, sala, seccion, cupos, anio, num_semestre, profesor, descriptor);
+		//agregarlo a un catalogo de ramos?
+	}
     
 }
--- AcademicManager/src/Ramo.java
+++ AcademicManager/src/Ramo.java
@@ -1,6 +1,31 @@
+import java.util.*;
+
 /**
  * Created by ahinri on 11-10-2015.
  */
 public class Ramo {
+
+	private String horario;
+	private String sala;
+	private int seccion;
+	private int cupos;
+	private int anio;
+	private int num_semestre;
+	private Profesor profesor;
+	private DescriptorRamo descriptor;
+	private List<Alumno> lista_alumnos;
+
+	public Ramo(String horario, String sala, int seccion, int cupos, int anio, int num_semestre, Profesor profesor, DescriptorRamo descriptor)
+	{
+		this.horario = horario;
+		this.sala = sala;
+		this.seccion = seccion;
+		this.cupos = cupos;
+		this.anio = anio;
+		this.num_semestre = num_semestre;
+		this.profesor = profesor;
+		this.descriptor = descriptor;
+		lista_alumnos = new ArrayList<Alumno>();
+	}
 	
 }
