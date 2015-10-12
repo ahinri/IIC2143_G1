e6283f4711dae3b8cfa6be25172ea34afb22ee35
EDUARDO VILDOSOLA <eavildosola@uc.cl>
2015-10-12 18:25:31 -0300

    mallas

--- AcademicManager/src/Malla.java
+++ AcademicManager/src/Malla.java
@@ -2,4 +2,15 @@
  * Created by ahinri on 11-10-2015.
  */
 public class Malla {
+	ramos=List
+	public Malla(String nombre){
+		this.nombre=nombre
+	
+	}
+	
+	public void agregar_ramos(Semestre s){
+		ramos.add(s)
+	}
+
 }
+
--- AcademicManager/src/Ramo.java
+++ AcademicManager/src/Ramo.java
@@ -2,4 +2,5 @@
  * Created by ahinri on 11-10-2015.
  */
 public class Ramo {
+	
 }
