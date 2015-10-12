f329ef612fcd083b490e8cd45ace7c539b7dd774
EDUARDO VILDOSOLA <eavildosola@uc.cl>
2015-10-12 19:34:26 -0300

    descriptor

--- AcademicManager/src/DescriptorRamo.java
+++ AcademicManager/src/DescriptorRamo.java
@@ -2,4 +2,16 @@
  * Created by ahinri on 11-10-2015.
  */
 public class DescriptorRamo {
+	int creditos;
+	String programa;
+	List<Ramo> requisitos=new List<Ramos>()
+	public DescriptorRamo(String sigla){
+		this.sigla=sigla
+	}
+	
+	public boolean agregarRequisitos(Ramo r){
+        requisitos.add(r)
+    }
+	
+	
 }
