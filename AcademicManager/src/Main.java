d240e3c63caa5046ada2aca3cf96887bca694ccf
ahinri <afhinrichsen@uc.cl>
2015-10-12 16:46:02 -0300

    a

--- AcademicManager/src/Main.java
+++ AcademicManager/src/Main.java
@@ -17,12 +17,22 @@ public class Main {
 			comparar los semestres que posee un historial con su malla asignada. El resultado de esto seria
 			algo parecido a lo que muestra el SidIng
 			2. Muchos metodos tienen tipo de retorno boolean por si el metodo llega a fallar
+			3. El profesor es quien califica. Si fuera el alumno, el requerimiento de que el alumno no puede
+			ingresar al sistema si reprobo cierta cantidad de ramos no tendria sentido (podria engañar al sistema)
 
 		 */
 
 		/*
 		CAMBIOS RESPECTO AL DIAGRAMA DE CLASES ACTUAL:
-			1. en HistorialAcademico se agrega el metodo obtenerAvance() para mostrar el avance curricular
+			1. En HistorialAcademico se agregan los metodos
+				obtenerAvance()
+				agregarSemstre();
+
+			2. En el diagrama, un Admin debiese tener flechas hacia las entidades que es capaz de crear. Esto
+			se debe a que, al usar el constructor de estas, conoce su estructura
+
+			3. A Ramo se le agrega el campo int id
+			4. En Semstre, el tipo de dato del campo notas se cambia a List<Double>
 		 */
 
 	}
